228b178be9a0090f4cff694731ee73839a1543f9
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-02-18 11:01:34 +0200

    add #save

--- lib/riddle.rb
+++ lib/riddle.rb
@@ -1,13 +1,30 @@
 class Riddle 
-  attr_reader :text
+  attr_reader :text, :answer, :id
 
-  def initialize(text, answer)
+  @@riddles = {}
+  @@total_rows = 0 
+
+  def self.all
+    @@riddles.values
+  end
+
+  def initialize(text, answer, id = nil)
     @text = text
     @answer = answer
+    @id = id
+  end
+
+  def save 
+    id = self.id || @@total_rows += 1
+    @@riddles[id] = Riddle.new(self.text, self.answer, id)
   end
 
   def correct? answer
     return @answer == answer
+  end
+
+  def == another_riddle
+    (self.text == another_riddle.text) and (self.answer == another_riddle.answer)
   end
 end
 
--- spec/riddle_spec.rb
+++ spec/riddle_spec.rb
@@ -11,6 +11,12 @@ describe '#Riddle' do
     @riddle = Riddle.new(@riddle_text, @riddle_answer)
   end
 
+  describe '.all' do
+    it 'is empty at the beginning' do
+      expect(Riddle.all).to eq []
+    end
+  end
+
   describe "#text" do
     it 'returns text of the riddle' do
       expect(@riddle.text).to eq @riddle_text
@@ -24,6 +30,25 @@ describe '#Riddle' do
 
     it 'returns `false` on wrong answer' do
       expect(@riddle.correct? 'no').to eq false
+    end
+  end
+
+  describe '#==' do
+    it 'true when text and answer are the same' do
+      riddle = Riddle.new('1+1', '2')
+      riddle2 = Riddle.new('1+1', '2')
+      expect(riddle == riddle2).to eq(true)
+    end 
+  end
+
+  describe '#save' do
+    it 'saved riddles can be obtained by .all method' do
+      riddle = Riddle.new('1+1', '2')
+      riddle2 = Riddle.new('1+2', '3')
+      riddle.save
+      riddle2.save
+
+      expect(Riddle.all).to eq [riddle, riddle2]
     end
   end
 end
